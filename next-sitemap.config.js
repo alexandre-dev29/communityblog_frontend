83e2529ce71cd65147c71428f166c1ab844de832
alexandre-dev29 <axel.business29@gmail.com>
2023-05-16 16:24:56 +0200

    adding sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "http://localhost:3000",
+  generateRobotsTxt: true, // (optional)
+  exclude: [
+    "/dashboard/*",
+    "/dashboard",
+    "/pages/dashboard",
+    "/dashboard/**/*",
+    "/pages/dashboard/*",
+    "/pages/dashboard/**/*",
+    "/server-sitemap.xml",
+  ],
+  sitemapSize: 7000,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${process.env.SITE_URL || "http://localhost:3000"}/server-sitemap.xml`,
+    ],
+    policies: [{ userAgent: "*", allow: "/", disallow: ["/dashboard"] }],
+  },
+};
